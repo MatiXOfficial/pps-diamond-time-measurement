2dcc9bf4b6926c05aa7e12bb00e01f48c98db3e1
Mateusz Kocot <mateuszkocot99@gmail.com>
2022-06-02 23:19:56 +0200

    Improved the architectures and added gaussian error

--- src/cfd_utils.py
+++ src/cfd_utils.py
@@ -40,6 +40,7 @@ def _gauss(x, a, mu, sigma):
     return a * np.exp(-(x - mu) ** 2 / (2 * sigma ** 2))
 
 
+# TODO - add new function for the histogram itself
 def find_diff_hist_stats(cfd: CFD | list[CFD], events: dict, show: bool = True, return_gauss_stats: bool = True,
                          hist_range: tuple[float, float] = (-0.5, 1.5), hist_alpha: float = 1., hist_label: str = None,
                          plot_gauss: bool = True, time_step: float = TIME_STEP):
@@ -63,6 +64,56 @@ def find_diff_hist_stats(cfd: CFD | list[CFD], events: dict, show: bool = True, 
         [calculate_event_cfd(cfd, events, i, shift=True, time_step=time_step, log=True) for i in range(N)])
     timestamps_diff = timestamps[:, 1] - timestamps[:, 0]
     hist_data = plt.hist(timestamps_diff, bins=100, range=hist_range, alpha=hist_alpha, label=hist_label)
+
+    # retrieve bins
+    bins_x, bins_y = hist_data[1][:-1], hist_data[0]
+    x_step = (bins_x[1] - bins_x[0]) / 2
+    bins_x += x_step
+
+    # regular statistics
+    weighted_stats = DescrStatsW(bins_x, weights=bins_y, ddof=0)
+    mean_stat = weighted_stats.mean
+    std_stat = weighted_stats.std
+
+    # fitted gaussian statistics
+    popt, _ = curve_fit(_gauss, bins_x, bins_y, p0=[1, mean_stat, std_stat])
+    gauss_mean = popt[1]
+    gauss_std = abs(popt[2])
+
+    if plot_gauss:
+        gauss_y = norm.pdf(bins_x, gauss_mean, gauss_std)
+        gauss_y *= np.max(bins_y) / np.max(gauss_y)
+        plt.plot(bins_x, gauss_y, 'r--', linewidth=2)
+
+    if show:
+        plt.show()
+
+    if return_gauss_stats:
+        return gauss_mean, gauss_std
+    else:
+        return mean_stat, std_stat
+    
+    
+def plot_diff_hist_stats(y_true, y_pred, show: bool = True, return_gauss_stats: bool = True, n_bins: int = 100,
+                         hist_range: tuple[float, float] = (-0.5, 1.5), hist_alpha: float = 1., hist_label: str = None,
+                         plot_gauss: bool = True):
+    """
+    Find the mean and std of a histogram of differences between cfd timestamps in two channels
+    :param y_true
+    :param y_pred
+    :param show: If True: the histogram is shown (plt.show())
+    :param return_gauss_stats: If True: the function returns the mean and std of a gaussian fitted to the histogram
+    :param n_bins
+    :param hist_range: Range of the histogram
+    :param hist_alpha: Alpha of the plotted histogram
+    :param hist_label: Label of the histogram
+    :param plot_gauss: If True: a fitted Gaussian is plotted with the histogram
+    :return: tuple: (mean, std) of the histogram
+    """
+    
+    # histogram
+    timestamps_diff = y_pred - y_true
+    hist_data = plt.hist(timestamps_diff, bins=n_bins, range=hist_range, alpha=hist_alpha, label=hist_label)
 
     # retrieve bins
     bins_x, bins_y = hist_data[1][:-1], hist_data[0]
