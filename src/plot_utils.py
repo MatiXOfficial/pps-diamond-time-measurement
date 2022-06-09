2fed08e90f329bd2b003ff7b875f24c7d919ce16
Mateusz Kocot <mateuszkocot99@gmail.com>
2022-06-09 22:39:24 +0200

    Moved plot_history to plot_utils.py

--- src/plot_utils.py
+++ src/plot_utils.py
@@ -22,3 +22,31 @@ def plot_sample(ampl: list[float], time: list[float] = None, title: str = '', ti
         plt.axvline(timestamp, c='red')
         title += f', t={timestamp:0.2f}'
     plt.title(title)
+
+
+def plot_history(history: dict[str, np.array], title: str, ymax: float = None):
+    """
+    Plot the loss history from training a neural network
+    :param history: dictionary with the data; history = model.fit(...).history
+    :param title: plot title
+    :param ymax: maximum of the plot
+    :return:
+    """
+    plt.figure(figsize=(10, 7))
+
+    X = np.arange(1, len(history['loss']) + 1)
+
+    plt.plot(X, history['loss'], label='train')
+    plt.plot(X, history['val_loss'], label='test')
+
+    if ymax is not None:
+        plt.ylim(0, ymax)
+
+    plt.xlabel('epoch')
+    plt.ylabel('loss')
+    plt.title(f"val loss {history[f'val_loss'].values[-1]:0.4f} (min: {min(history[f'val_loss'].values):0.4f})")
+    plt.grid()
+    plt.legend()
+
+    plt.suptitle(title)
+    plt.show()
